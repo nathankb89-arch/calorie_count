b43ff8e99c279c69224b099c82dd72c55d49202e
nate <nathankb89@gmail.com>
2026-07-05 19:59:15 +0300

    Implement calorie tracking views

--- calorieApp/views.py
+++ calorieApp/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Food
 
 # Create your views here.
+def index(request):
+    if request.method == "POST":
+        name = request.POST['name']
+        calories = request.POST['calories']
+
+        Food.objects.create(
+            name=name,
+            calories=calories
+        )
+
+        return redirect('/')
+
+    foods = Food.objects.all()
+
+    total = sum(food.calories for food in foods)
+
+    return render(request, 'index.html', {
+        'foods': foods,
+        'total': total
+    })
+
+
+def delete_food(request, id):
+    food = Food.objects.get(id=id)
+    food.delete()
+
+    return redirect('/')
+
+
+def reset(request):
+    Food.objects.all().delete()
+    return redirect('/')

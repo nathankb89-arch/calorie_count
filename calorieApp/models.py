56d91ecddfbbfd983bf99b2ea368bc91e1f98364
nate <nathankb89@gmail.com>
2026-07-05 19:52:14 +0300

    Create Food model

--- calorieApp/models.py
+++ calorieApp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Food(models.Model):
+    name = models.CharField(max_length=100)
+    calories = models.IntegerField()
+
+    def __str__(self):
+        return self.name
